2a63194974a37931ef0f5e992f0cd18a50b9b87f
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-09 08:49:58 -0400

    Add project files.

---
+++ Topic 2 - Numbers and Math/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Topic_2___Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I will now count my chickens:");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+        }
+    }
+}
