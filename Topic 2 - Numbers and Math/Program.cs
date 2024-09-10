84ec0a31b2e268f63ccf6c721d1f70ed99ecf2d9
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-10 08:48:38 -0400

    Working

--- Topic 2 - Numbers and Math/Program.cs
+++ Topic 2 - Numbers and Math/Program.cs
@@ -8,6 +8,26 @@
             Console.WriteLine("I will now count my chickens:");
             Console.WriteLine("Hens " + (10 + 5 * 2));
             Console.WriteLine("Roosters " + (25 + 30 / 6));
+            Console.WriteLine("six" + 6);
+            Console.WriteLine("6 + 6");
+            Console.WriteLine("6" + 6);
+            Console.WriteLine(6 + 6);
+            Console.WriteLine();
+
+            Console.WriteLine("Now I will count the eggs:");
+            Console.WriteLine(11.0 / 2);
+            Console.WriteLine();
+
+            Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine("What is 3 + 2? " + (3 + 2));
+            Console.WriteLine("What is 5 - 7? " + (5 - 7));
+            Console.WriteLine("Oh, that's why it's " + (3 + 2 < 5 - 7));
+            Console.WriteLine();
+
+            
+
+            
         }
     }
 }
